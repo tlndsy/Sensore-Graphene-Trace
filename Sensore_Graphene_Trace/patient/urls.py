9c9015a3b31c0c1ee3d6af01ba85971a0e0696c5
EuanMacLeod <em1026@student.aru.ac.uk>
2026-02-18 14:28:47 +0000

    Created Initial Patient Splash Page

--- Sensore_Graphene_Trace/patient/urls.py
+++ Sensore_Graphene_Trace/patient/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'patient'
+
 urlpatterns = [
+    path("", views.home, name="home"),
     path("stats/", views.stats, name="stats"),
+    path("logout/", views.temp_logout, name="logout"),
 ]
--- Sensore_Graphene_Trace/patient/views.py
+++ Sensore_Graphene_Trace/patient/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth import login, logout
+from django.contrib.auth.decorators import login_required
+
+from user.models import User, Message
 
 # Create your views here.
+@login_required(login_url='/user/home/')
+def home(request):
+    user = request.user
+    notifications = len(Message.objects.filter(recipient=user, read_receipt=False))
+
+
+    return render(request,'patient/home.html', {"user": user, "notifications": notifications})
+
 def stats(request):
     return HttpResponse("This is the patients stats page (e.g., graph, heatmap")
+
+def temp_logout(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect("user:home")
