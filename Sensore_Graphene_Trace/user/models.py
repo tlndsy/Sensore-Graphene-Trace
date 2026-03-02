5a85420d6acfaa36d3a5bd6d55e54aac3027eed1
EuanMacLeod <em1026@student.aru.ac.uk>
2026-03-02 16:33:52 +0000

    changed the user save() method to a signal

--- Sensore_Graphene_Trace/user/models.py
+++ Sensore_Graphene_Trace/user/models.py
@@ -91,14 +91,6 @@ class User(AbstractBaseUser, PermissionsMixin):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    def save(self, *args, **kwargs):
-        is_new = self.pk is None
-        super().save(*args, **kwargs)
-
-        if is_new:
-            group, _ = Group.objects.get_or_create(name=self.role)
-            self.groups.add(group)
-
     def __str__(self):
         return self.email
 
