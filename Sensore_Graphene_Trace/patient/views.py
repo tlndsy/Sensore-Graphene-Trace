024be452c5e4e02f065c8275990a5e982094f631
Starlight667 <146363824+Starlight677@users.noreply.github.com>
2026-03-27 11:32:29 +0000

    Added error handling for the report page - it now informs user there are no scans found rather than crashing.

--- Sensore_Graphene_Trace/patient/views.py
+++ Sensore_Graphene_Trace/patient/views.py
@@ -111,26 +111,37 @@ def interpreterDisplay(request):
         currentPage = 0
     elif currentPage >= noOfReadings:
         currentPage = noOfReadings - 1
-    print(currentPage)
-    current_reading = all_readings[currentPage]
 
-    file = current_reading.pressure_reading
-    if not Report.objects.filter(pressure_map_reading=current_reading).exists():
-        # Make a new report only if one does not already exist
-        report = Report(pressure_map_reading=current_reading)
-        reportContents, scanNumber = ScanInterpreter.runInterpreter(ScanInterpreter, file)
-        report.content = "@".join(reportContents)
-        report.frame = scanNumber
-        report.save()
-    else:
-        report = Report.objects.filter(pressure_map_reading=current_reading).last()
+    try:
+        current_reading = all_readings[currentPage]
+        file = current_reading.pressure_reading
 
-    frameHeatmap = ScanInterpreter.get_pressure_matrix(ScanInterpreter, file, report.frame) # Currently not working
-    reportContents = report.content.split("@")
+        if not Report.objects.filter(pressure_map_reading=current_reading).exists():
+            # Make a new report only if one does not already exist
+            report = Report(pressure_map_reading=current_reading)
+            reportContents, scanNumber = ScanInterpreter.runInterpreter(ScanInterpreter, file)
+            report.content = "@".join(reportContents)
+            report.frame = scanNumber
+            report.save()
+        else:
+            report = Report.objects.filter(pressure_map_reading=current_reading).last()
 
-    context = {"report_0": reportContents[0], "report_1": reportContents[1], "report_2": reportContents[2],
-               "report_3": reportContents[3], "reportNumber": currentPage+1, "noOfReports": noOfReadings,
-               "heatmap": frameHeatmap, "allReports": all_readings}
+        frameHeatmap = ScanInterpreter.get_pressure_matrix(ScanInterpreter, file, report.frame) # Currently not working
+        reportContents = report.content.split("@")
+
+        context = {"report_0": reportContents[0], "report_1": reportContents[1], "report_2": reportContents[2],
+                   "report_3": reportContents[3], "reportNumber": currentPage+1, "noOfReports": noOfReadings,
+                   "heatmap": frameHeatmap, "allReports": all_readings}
+
+    except Exception: #If no scans found, inform user of this
+        reportContents = ["","","",""]
+
+        reportContents[0] = "No scans have been detected for this patient."
+        reportContents[1] = "Please contact your administrator if you believe this is in error."
+
+        context = {"report_0": reportContents[0], "report_1": reportContents[1], "report_2": reportContents[2],
+                   "report_3": reportContents[3], "reportNumber": 0, "noOfReports": 0, "allReports": all_readings}
+
     return render(request, "patient\interpreterDisplay.html", context)
 
 def interpreterButton(request):
