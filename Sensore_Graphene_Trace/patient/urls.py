9b355f14378eeb557509c1bfbd96e957731ae022
sahid <sahidnehran@gmail.com>
2026-04-16 11:00:30 +0100

    final merge

--- Sensore_Graphene_Trace/patient/urls.py
+++ Sensore_Graphene_Trace/patient/urls.py
@@ -9,15 +9,15 @@ app_name = 'patient'
 
 urlpatterns = [
     path("", views.PatientHomeView.as_view(), name="home"),
-    path('upload/', views.upload_csv, name='upload_csv'),
     path("profile/", views.profile, name="profile"),
     path("view-devices/", views.PatientViewDevices.as_view(), name="viewDevices"),
     path("register-device/", views.PatientRegisterDeviceView.as_view(), name="registerDevice"),
     path("stats/", views.stats, name="stats"),
     path("report/", views.interpreterDisplay, name="report"),
-    path("notifications/", views.notifications, name="stats"),
-    path("messages/", views.messages, name="stats"),
+    path("notifications/", views.notifications, name="notifications"),
+    path("messages/", views.messages, name="messages"),
     path("logout/", views.temp_logout, name="logout"),
     path("graphs/", views.view_graph, name="graphs"),
     path("report/patient-button", views.interpreterButton, name="interpreterButton"),
+
 ]
